844e06cba54bd62008dffb69344b6d944bb3866d
Dennis <codefree@protonmail.com>
2019-12-23 00:03:53 +0100

    add: basic test structure

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default () => {
-  return <div>Comment Box</div>;
+  const [comment, setComment] = useState('');
+
+  const submitComment = e => {
+    e.preventDefault();
+    setComment('');
+  };
+
+  const handleChange = e => {
+    setComment(e.target.value);
+  };
+
+  return (
+    <form onSubmit={submitComment} action="">
+      <h4>Add a Comment</h4>
+      <textarea
+        onChange={handleChange}
+        value={comment}
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
+      <div>
+        <button>Submit Comment</button>
+      </div>
+    </form>
+  );
 };
