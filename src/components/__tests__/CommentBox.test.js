e2804c83467caf771e0d757cee25f63f79d7bfae
Dennis <DennisLoska@users.noreply.github.com>
2019-12-23 00:51:17 +0100

    fix: refactor CommentBox with describe() block

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,26 +17,27 @@ it('has a textarea and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea you can type in', () => {
-  wrapped
-    .find('textarea')
-    //simulate change event
-    .simulate('change', { target: { value: 'new comment' } })
-    //simulate state update
-    .update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
+describe('the text area', () => {
+  beforeEach(() => {
+    //enter some text into the textarea
+    wrapped
+      .find('textarea')
+      //simulate change event
+      .simulate('change', { target: { value: 'new comment' } })
+      //simulate state update
+      .update();
+  });
 
-it('submits a comment and clears the textarea', () => {
-  //enter some text into the textarea
-  wrapped
-    .find('textarea')
-    .simulate('change', { target: { value: 'new comment' } })
-    .update();
-  //fire the submit event and assert the expected value
-  wrapped
-    .find('form')
-    .simulate('submit')
-    .update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+  it('has a textarea you can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('submits a comment and clears the textarea', () => {
+    //fire the submit event and assert the expected value
+    wrapped
+      .find('form')
+      .simulate('submit')
+      .update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
