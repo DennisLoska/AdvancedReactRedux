3a9cc487ea18cc86cc7c987a4b89227b5bf450c6
Dennis <DennisLoska@users.noreply.github.com>
2019-12-23 23:40:25 +0100

    add: complete integration test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+let wrapped;
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Comment 1' }, { name: 'Comment 2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+  wrapped.unmount();
+});
+
+it('can fetch a list of comments and display them', done => {
   //Attempt to render the entire app
-  const wrapped = mount(
+  wrapped = mount(
     <Root>
       <App />
     </Root>
   );
-
   //find the button to fetch the comments
   wrapped.find('.fetch-comments').simulate('click');
-
-  //Expect to find a list of comments
-  expect(wrapped.find('li').length).toEqual(500);
+  //Add delay to wait for moxios to kick in
+  moxios.wait(() => {
+    wrapped.update();
+    //Expect to find a list of comments
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+  }, 100);
 });
