9d3e20bb69d4690308d149f1ca2e944a7c8ec869
Dennis <DennisLoska@users.noreply.github.com>
2019-12-23 22:27:18 +0100

    add: action type & creator to fetch comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
 import { SAVE_COMMENT } from 'actions/types';
+import { FETCH_COMMENTS } from 'actions/types';
 
 export const saveComment = comment => {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  };
+};
+
+export const fetchComments = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const comments = await response.json();
+  return {
+    type: FETCH_COMMENTS,
+    payload: comments
   };
 };
