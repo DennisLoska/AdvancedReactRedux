9b7904559ba8e2239a433b9ea21858fcece0324f
Dennis <DennisLoska@users.noreply.github.com>
2019-12-24 01:16:46 +0100

    add: auth creator and reducer for HOC

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
-import { SAVE_COMMENT } from 'actions/types';
-import { FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios';
 
 export const saveComment = comment => {
@@ -19,5 +18,12 @@ export const fetchComments = async () => {
   return {
     type: FETCH_COMMENTS,
     payload: comments.data //comments using fetch API
+  };
+};
+
+export const changeAuth = isLoggedIn => {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
   };
 };
