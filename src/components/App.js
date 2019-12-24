1befe7779a407d985282a8016926c1debdaefd6e
Dennis <DennisLoska@users.noreply.github.com>
2019-12-24 01:17:33 +0100

    refactor: prepare refactor for HOC requiredAuth

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default () => {
+const App = props => {
+  const renderButton = () => {
+    if (props.auth) return <buuton>Sign out</buuton>;
+    else return <button>Sign In</button>;
+  };
+
+  const renderHeader = () => {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>{' '}
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>{' '}
+        </li>
+        <li>{renderButton()}</li>
+      </ul>
+    );
+  };
+
   return (
     <div>
-      <CommentBox/>
-      <CommentList/>
+      {renderHeader()}
+      <Route path="/post" component={CommentBox} />
+      <Route path="/" exact component={CommentList} />
     </div>
   );
 };
+
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(App);
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -10,6 +10,7 @@ const CommentList = props => {
 
   return (
     <div>
+      <h4>Comment List</h4>
       <ul>{renderComments()}</ul>
     </div>
   );
