a8166e23027d1e660d5ca272838fb8546231e463
Dennis <DennisLoska@users.noreply.github.com>
2020-07-13 21:54:25 +0200

    add initial auth components

---
+++ auth/client/src/components/auth/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <form>
+      <fieldset>
+        <label htmlFor="email">Email</label>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="password">Password</label>
+      </fieldset>
+    </form>
+  </div>
+);
