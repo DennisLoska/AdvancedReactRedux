9aaef5e3b2ec866ce6ce5386a5f727b54b866c48
Dennis <DennisLoska@users.noreply.github.com>
2020-07-13 22:08:20 +0200

    add redux to project

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import reducers from './reducers';
 import App from './components/App.jsx';
 import Welcome from './components/Welcome.jsx';
 import SignUp from './components/auth/SignUp.jsx';
@@ -9,13 +12,16 @@ import SignOut from './components/auth/SignOut.jsx';
 import Feature from './components/Feature.jsx';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" exact component={SignUp} />
-      <Route path="/signin" exact component={SignIn} />
-      <Route path="/signout" exact component={SignOut} />
-      <Route path="/feature" exact component={Feature} />
-    </App>
-  </BrowserRouter>, document.querySelector('#root'),
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signout" exact component={SignOut} />
+        <Route path="/feature" exact component={Feature} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root'),
 );
