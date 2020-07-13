29a8b0b3e45e4b79784d5df9896ecc7479ba9d38
Dennis <DennisLoska@users.noreply.github.com>
2020-07-13 22:17:47 +0200

    add form fields from redux form

--- auth/client/src/components/auth/SignUp.jsx
+++ auth/client/src/components/auth/SignUp.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default () => (
+const SignUp = () => (
   <div>
     <form>
       <fieldset>
         <label htmlFor="email">Email</label>
+        <Field
+          name="email"
+          type="text"
+          component="input"
+          autoComplete="none"
+        />
       </fieldset>
       <fieldset>
         <label htmlFor="password">Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
       </fieldset>
     </form>
   </div>
 );
+
+export default reduxForm({ form: 'signup' })(SignUp);
